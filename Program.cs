121be7cbc73840324ea9a0c895a5bd35375908fb
GotoTop <92088003+GotToTop@users.noreply.github.com>
2022-10-03 18:10:24 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,1 +1,8 @@
 Console.WriteLine("Hello, World! Sorting algorithm initialization...");
+
+int[] intArray = new int[] { 9, 2, 4, 3, 1, 5 };
+var maxVal = intArray.Max();
+var rng = new Random();
+for (int i = 0; i < intArray.Length; i++) { 
+  intArray[i] = rng.Next(0, maxVal+1);
+}
